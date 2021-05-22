065a7c3f5630ae02477f5511e4c6bc83baa38660
Vinibrahj <75343560+Vinibrahj@users.noreply.github.com>
2021-05-22 02:08:00 -0300

    Commit Inicial

---
+++ OngCrud/Controllers/FuncionarioController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OngCrud.Models;
+
+namespace OngCrud.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        // GET: Funcionario
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (DBModel db = new DBModel())
+            {
+                List<Funcionario> funcList = db.Funcionarios.ToList<Funcionario>();
+                return Json(new { data = funcList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            return View(new Funcionario());
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(Funcionario fnc)
+        {
+            using (DBModel db = new DBModel())
+            {
+                db.Funcionarios.Add(fnc);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Salvo com sucesso!" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ OngCrud/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OngCrud.MvcApplication" Language="C#" %>
